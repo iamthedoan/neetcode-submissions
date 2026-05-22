1e311b955888f43074ba461a0b232ac7ed26b0c7
Brandon <52016293+iamthedoan@users.noreply.github.com>
2026-05-22 13:24:38 -0700

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,32 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        # hash map everythhign then compare the hashmaps
+        # seen = set()
+        ana_dict = {}
+        # store unique combos in hash, record indices
+
+        for i, word in enumerate(strs):
+            word_dict = {}
+            for c in word:
+                word_dict[c] = 1 + word_dict.get(c,0)
+            
+            key = tuple(sorted(word_dict.items()))
+            if key in ana_dict:
+                ana_dict[key].append(i)
+            else:
+                ana_dict[key] = [i]
+
+        ana_list = []
+        for val in ana_dict.values():
+            ana_list.append([strs[i] for i in val])
+
+        return ana_list
+
+
+            
+
+
+
+
+        
